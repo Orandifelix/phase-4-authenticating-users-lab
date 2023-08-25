563f37a4959b5d6d83d4e9b9ef3a69c333a7919e
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-08-25 03:26:06 +0300

    more changes  on  the routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,38 @@
 class UsersController < ApplicationController
-  def create
-    user = User.create!(user_params)
 
-    if user
-      session[:user_id] = user.id
-      render json: user, status: :created
-    else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-    end
+  def show
+  user = User.find_by(id: session[:user_id])
+  if user
+    render json: user
+  else
+    render json: { errors:  'User not found' }, status: 401
   end
+  # def create
+  #   user = User.new(user_params)
 
-    def show
-        user = User.find_by(id: session[:user_id])
+  #   if user.save
+  #     session[:user_id] = user.id
+  #     render json: user, status: :created
+  #   else
+  #     render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
 
-        if user
-        render json: user
-        else
-        render json: { error: 'User not found' }, status: :not_found
-        end
-    end
+  #   def show
+  #       user = User.find_by(id: session[:user_id])
+
+  #       if user
+  #       render json: user
+  #       else
+  #       render json: { error: 'User not found' }, status: :not_found
+  #       end
+  #   end
 
 
-  private
+  # private
 
-  def user_params
-    params.permit(:username)
-  end
+  # def user_params
+  #   params.permit(:username)
+  # end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/me', to: 'users#show'
-  post '/me', to: 'users#create'
+  # post '/login', to: 'sessions#create'
+  # delete '/logout', to: 'sessions#destroy'
+  # get '/me', to: 'users#show'
+  # # post '/me', to: 'users#create'
   
+
+  get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
